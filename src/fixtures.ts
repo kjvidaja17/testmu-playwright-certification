60108c9e59eb8d5c2a553c73effeb80e7ba08a7c
kjvidaja17 <kjvidaja17@users.noreply.github.com>
2026-07-14 14:44:40 +0800

    chore: track missing profile files and add test-results to ignore

--- src/fixtures.ts
+++ src/fixtures.ts
@@ -3,12 +3,14 @@ import { test as base } from '@playwright/test';
 import { ShopPage } from './pages/ShopPage';
 import { ProductPage } from './pages/ProductPage';
 import { CartPage } from './pages/CartPage';
+import { ProfilePage } from './pages/ProfilePage';
 
 // 2. Your custom type declaration applied flawlessly!
 type MyPages = {
     shopPage: ShopPage;
     productPage: ProductPage;
     cartPage: CartPage;
+    profilePage: ProfilePage;
 };
 
 // 3. Extend the base Playwright test object to include your custom fixtures
@@ -25,6 +27,9 @@ export const test = base.extend<MyPages>({
         // Automatically creates an instance of CartPage when called
         await use(new CartPage(page));
     },
+    profilePage: async ({ page }, use) => {
+        await use(new ProfilePage(page));
+    }
 });
 
 // 4. Export the base expect object so everything can be imported from one single file
